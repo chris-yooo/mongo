42b11ba74b9d9daa416c020399397afe62ea4445
chris_yooo <hhxtreams@gmail.com>
2022-07-07 17:39:38 +0200

    yo

--- api/users.js
+++ api/users.js
@@ -1,4 +1,26 @@
-const handler = (request, response) => {
-  return response.json({ message: "Ich bin die /users Route" });
+import connectToMongoDb from "./_database/connect-to-mongodb";
+import User from "./_database/models/User";
+
+const handler = async (request, response) => {
+  try {
+    await connectToMongoDb();
+
+    if (request.method === "GET") {
+      const users = await User.find({});
+      return response.json(users);
+    } else if (request.method === "POST") {
+      const user = new User({
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        age: request.body.age,
+      });
+      const newUser = await user.save();
+      return response.json(newUser);
+    } else {
+      throw new Error("Route not supported");
+    }
+  } catch (error) {
+    return response.status(500).json({ message: error.message });
+  }
 };
 export default handler;
